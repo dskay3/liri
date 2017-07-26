294af07f944ded85420aa58d023992ea84b0c275
Daniel <dskay3@gmail.com>
2017-07-26 00:07:24 -0400

    Removed assets folder.

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 // Exports of tokens & keys
-const twitterKeys = require("assets/keys.js").twitterKeys;
-const spotifyKeys = require("assets/keys.js").spotifyKeys;
+const twitterKeys = require("./keys.js").twitterKeys;
+const spotifyKeys = require("./keys.js").spotifyKeys;
 
 var nodeArgs = process.argv;
 var command = process.argv[2];
