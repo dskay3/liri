602aeed5129a684b62b9275fa960e4d46e34a575
Daniel <dskay3@gmail.com>
2017-07-26 01:37:23 -0400

    Added twitter retrieval functionality to liri.

--- liri.js
+++ liri.js
@@ -8,6 +8,7 @@ var command = process.argv[2];
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
 const chalk = require('chalk');
+var moment = require('moment');
 
 var twitterClient = new Twitter(twitterKeys);
 var spotifyClient = new Spotify(spotifyKeys);
@@ -22,16 +23,17 @@ var commands = ['my-tweets', 'spotify-this-song', 'movie-this', 'do-what-it-says
 
 // Constants
 const dashes = '-----------------------------------------------------';
+const spaces = "    ";
 
 // Displays instructions
 function instructions() {
     console.log(dashes);
     console.log("Hello. I am Liri.\n");
-    console.log("Type the following into the terminal " + chalk.green("node liri.js") + " followed by one of the following commands");
-    console.log(chalk.bgBlue("my-tweets"));
-    console.log(chalk.bgGreen("spotify-this-song"));
-    console.log(chalk.bgRed("movie-this"));
-    console.log(chalk.bgMagenta("do-what-it-says"));
+    console.log("Type the following into the terminal " + chalk.green("node liri.js") + " followed by one of the following commands:");
+    console.log(spaces + chalk.bgBlue("my-tweets"));
+    console.log(spaces + chalk.bgGreen("spotify-this-song"));
+    console.log(spaces + chalk.bgRed("movie-this"));
+    console.log(spaces + chalk.bgMagenta("do-what-it-says"));
     console.log(dashes);
 };
 
@@ -42,11 +44,29 @@ if (command == commands[0]) {
 }
 
 function myTweets() {
-    console.log("Retrieving the last 20 tweets.");
-
     twitterClient.get('statuses/user_timeline', params, function(error, tweets, response) {
         if (!error) {
-            console.log(tweets);
+            var count = 0;
+
+            // Displays screen name
+            console.log("Retrieving " + chalk.bold.green(tweets[0].user.screen_name) + " tweets.");
+            console.log(dashes);
+
+            setTimeout(function() {
+                // Go through and logs each tweet data
+                tweets.forEach(function(tweet) {
+                    console.log("Tweet Generated On: " + moment(tweet.created_at, "ddd MMM D HH:mm:ss ZZ YYYY").format("dddd, MMMM Do YYYY, h:mm:ss a"));
+                    console.log("Tweet Text: " + chalk.blue(tweet.text));
+                    console.log("Number of Times Favorited: " + tweet.favorite_count);
+                    console.log(dashes);
+                    count++;
+                });
+            }, 1500);
+            
+            setTimeout(function() {
+                console.log(chalk.bgRed("Tweets data retrieval has been completed."));
+                console.log(chalk.bold(count + " tweets were retrieved."));
+            }, 3000);
         }
     });
 }
